40adfbcdb64852668e4427c04d8b11b6a61ec30a
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2024-01-21 17:32:39 +0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,68 @@
 import sqlite3
 
+# 1 create the database name test
 conn = sqlite3.connect('test.db')
-
 print("Opened database successfully")
+
+# 2 create the table name COMPANY
+# conn.execute('''CREATE TABLE COMPANY
+#          (ID INT PRIMARY KEY     NOT NULL,
+#          NAME           TEXT    NOT NULL,
+#          AGE            INT     NOT NULL,
+#          ADDRESS        CHAR(50),
+#          SALARY         REAL);''')
+# print("Table created successfully")
+# conn.close()
+
+# 3 insert values into table name COMPANY
+# conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+#       VALUES (5, 'Paul', 32, 'California', 123.00 )")
+#
+# conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+#       VALUES (6, 'Allen', 25, 'Texas', 15000.00 )")
+#
+# conn.commit()
+# print("Records created successfully")
+# conn.close()
+
+# 4 read data form table name COMPANY
+# cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
+# for row in cursor:
+#     print("ID = ", row[0])
+#     print("NAME = ", row[1])
+#     print("ADDRESS = ", row[2])
+#     print("SALARY = ", row[3]), "\n"
+#
+# print("Operation done successfully")
+# conn.close()
+
+# 5 update values of database name COMPANY
+# value = "1"
+# conn.execute(f"UPDATE COMPANY set SALARY = 5000.00 where ID = {value}")
+# conn.commit()
+# print("Total number of rows updated :", conn.total_changes)
+#
+# cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
+# for row in cursor:
+#     print("ID = ", row[0])
+#     print("NAME = ", row[1])
+#     print("ADDRESS = ", row[2])
+#     print("SALARY = ", row[3]), "\n"
+#
+# print("Operation done successfully")
+# conn.close()
+
+# 6 delete values
+# conn.execute("DELETE from COMPANY where ID = 5;")
+# conn.commit()
+# print("Total number of rows deleted :", conn.total_changes)
+#
+# cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
+# for row in cursor:
+#     print("ID = ", row[0])
+#     print("NAME = ", row[1])
+#     print("ADDRESS = ", row[2])
+#     print("SALARY = ", row[3]), "\n"
+#
+# print("Operation done successfully")
+# conn.close()
